19f8c34f83fa54377a46c24367293667eae336a6
Aditya Bayu <aditadityabayuaji@gmail.com>
2018-12-22 12:47:18 +0700

    Update anything

--- js/portfolio-detail.js
+++ js/portfolio-detail.js
@@ -1,4 +1,33 @@
 $(function(){
+	function bottomNavHandler (target){
+		if (target == "#section-4" || target == "section-4"){
+			$(".bottom-navigation").addClass("show")
+			$(".bottom-navigation").removeClass("hidden")
+		} else {
+			$(".bottom-navigation").removeClass("show")
+			$(".bottom-navigation").addClass("hidden")
+		}
+	}
+
+	function indicatorHandler (target){
+		if (target !== "#section-1" && target !== "section-1"){
+			$(".indicator a").addClass("c-blue-charcoal")
+		} else {
+			$(".indicator a").removeClass("c-blue-charcoal")
+		}
+	}
+
+	var myFullpage = new fullpage('#portfolio-detail-01', {
+			// sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+			afterLoad: function(origin, destination, direction){
+				bottomNavHandler(destination.item.id)
+			}, 
+			onLeave: function(origin, destination, direction){
+				console.log(destination);
+				indicatorHandler(destination.item.id)
+			}
+		});
+
 	$(".cta").click(function(){
 		var target = $(this).data("target") //-> mengambil data dari action selector (target) = "#portfolio-detail-01(bisa beda beda tergantung data target)"
 		$(target).show()
@@ -9,9 +38,37 @@ $(function(){
 
 	})
 
+	$(".indicator a").click(function(e){
+		e.preventDefault();
+		var target = $(this).attr('href');
+		var position = $(target).offset().top
+		// console.log(target)
 
+		$('html body').animate(
+			{
+				scrollTop: position, //-->parameter: syarat biar animasinya jalan, apa yg dianimasiin misal kasi margin top dll
+			},
+			{
+				duration: 500, //-->option: confignya animasi
+				complete: function() {
+					bottomNavHandler(target)
+					
+				}
+			}
+		);
 
+		indicatorHandler(target)
 
+		
+		$(".indicator a").removeClass("active")
+		$(this).addClass("active")
+
+		$(".nav-fixed").addClass("nav-scrolled")
+	})
+
+	$(".indicator a").first().click(function(){
+		$(".nav-fixed").removeClass("nav-scrolled")
+	})
 
 	
 })
